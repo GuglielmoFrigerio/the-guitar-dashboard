3e637025e322d1066a2649c3045935fe5dab4de9
Guglielmo Frigerio <guglielmo.frigerio@gmail.com>
2021-08-03 17:24:56 +0200

    wip on CorrelationSlot

---
+++ Source/CorrelationSlot.h
@@ -0,0 +1,16 @@
+/*
+  ==============================================================================
+
+    CorrelationSlot.h
+    Created: 3 Aug 2021 5:14:55pm
+    Author:  gugli
+
+  ==============================================================================
+*/
+
+#pragma once
+
+struct CorrelationSlot {
+    int     sampleCount;
+    float   correlationSum;
+};
--- Source/PitchDetector.h
+++ Source/PitchDetector.h
@@ -2,13 +2,17 @@
 #include <memory>
 #include <cmath>
 #include "AudioBuffer.h"
+#include "CorrelationSlot.h"
 
 class PitchDetector {
 private:	// private fields
 	std::unique_ptr<AudioBuffer> m_sampleBufferPtr;
 	const int MinimumMidiNote = 40;
+	const int MaximumMidiNote = 77;
 	const float ReferenceFrequency = 440.0;
 	float m_noiseGateThreashold;
+	CorrelationSlot* m_pCorrelationSlots;
+	int	m_correlationSlots;
 
 private:	// implementation
 	float computeVolume(int sampleCount);
