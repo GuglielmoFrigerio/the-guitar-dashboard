2c2a5b9725af0d65a2a028f7f115d76ca15ef36d
Guglielmo Frigerio <guglielmo.frigerio@gmail.com>
2021-09-02 17:46:50 +0200

    wip play component

---
+++ Source/PlayerComponent.h
@@ -0,0 +1,22 @@
+/*
+  ==============================================================================
+
+    PlayerComponent.h
+    Created: 2 Sep 2021 2:27:51pm
+    Author:  gugli
+
+  ==============================================================================
+*/
+
+#pragma once
+#include <JuceHeader.h>
+
+class PlayerComponent : public juce::Component 
+{
+private:    // fields
+
+public:
+
+    void paint(juce::Graphics&) override;
+    void resized() override;
+};
